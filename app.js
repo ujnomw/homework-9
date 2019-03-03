9ea82b10df4bc8f4e9e12dd657986c7f571b44fd
ujnomw <m.golovatskih@gmail.com>
2019-03-03 14:06:18 +0300

    Styles added and search fixed

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-const searchField = document.getElementById('search').firstChild;
+const searchField = document.getElementById('search').firstElementChild;
 const searchButton = document.getElementById('search').lastElementChild;
 const resultsField = document.getElementById('results');
-const wikiArticle = 'https://ru.wikipedia.org/wiki/';
 function fetchResults(searchQuery) {
-const endpoint = `https://ru.wikipedia.org/w/api.php?action=query&list=search&prop=
-info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
+// const endpoint = `https://ru.wikipedia.org/w/api.php?action=query&list=search&prop=                 alternative
+// extracts&explaintext&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;     search query
+const endpoint = `https://ru.wikipedia.org/w/api.php?action=opensearch&search=${searchQuery}`;
 fetch(endpoint)
-.then(response => { return response.json()})
-.then(data => {
-const results = data.query.search;
-console.log(results);
-let output = '<ol>';
-results.forEach((el) => {
-  output += '<li> <a href=' + wikiArticle + encodeURI(el.title) + '>'+ el.title + '</a> </li>' ;
-})
-resultsField.innerHTML = output + '</ol>';
-});
+  .then(response => { return response.json()})
+  .then(results => {
+    let output = '<ul>';
+    results[1].forEach((el, i) => {                               //вывод результатов поиска
+      output += '<li> <a href='+results[3][i]+'>'+ el + '</a>'+
+      '<p>' + results[2][i] +'</p>' + '</li>' ;
+    })
+resultsField.innerHTML = output + '</ul>';
+  });
 }
 searchButton.addEventListener('click', ()=> {fetchResults(searchField.value)})
