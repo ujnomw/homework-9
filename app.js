4db7c379eab29744eff264102e5e13c2b6c5dbc6
ujnomw <m.golovatskih@gmail.com>
2019-03-03 11:09:52 +0300

    Links to articles added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const searchField = document.getElementById('search').firstChild;
 const searchButton = document.getElementById('search').lastElementChild;
 const resultsField = document.getElementById('results');
+const wikiArticle = 'https://ru.wikipedia.org/wiki/';
 function fetchResults(searchQuery) {
 const endpoint = `https://ru.wikipedia.org/w/api.php?action=query&list=search&prop=
 info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
@@ -11,7 +12,7 @@ const results = data.query.search;
 console.log(results);
 let output = '<ol>';
 results.forEach((el) => {
-  output += '<li> ' + el.title + ' </li>' ;
+  output += '<li> <a href=' + wikiArticle + encodeURI(el.title) + '>'+ el.title + '</a> </li>' ;
 })
 resultsField.innerHTML = output + '</ol>';
 });
